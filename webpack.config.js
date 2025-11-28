feadb54e17b37033fda7f3372144d51b34914816
Reeennooo <renat.kurkin@mail.ru>
2025-11-28 17:33:36 +0300

    github pages

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
     clean: true,
+    publicPath: "/historical-dates/"
   },
   resolve: {
     extensions: [".tsx", ".ts", ".js", ".jsx"],
